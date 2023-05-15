354c634570fed96a183fc5625a082edd09d08f1f
test <>
2023-05-15 19:59:16 +0800

    exceptionDemo

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,10 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
+      try {
+        CartClass cart = CartClass();
+        cart.addItem(null); // Throws NullArgumentException
+      } catch (e) {}
       _counter++;
     });
   }
