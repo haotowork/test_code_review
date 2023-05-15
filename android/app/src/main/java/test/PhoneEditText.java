b14b76ea3228468bff1ef89f8efd5f698a167229
test <>
2023-05-15 17:40:48 +0800

    PhoneEditText

---
+++ android/app/src/main/java/test/PhoneEditText.java
@@ -0,0 +1,75 @@
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.EditText;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+public class PhoneEditText extends AppCompatEditText implements TextWatcher {
+
+    private static final int PHONE_INDEX_3 = 3;
+    private static final int PHONE_INDEX_4 = 4;
+    private static final int PHONE_INDEX_8 = 8;
+    private static final int PHONE_INDEX_9 = 9;
+
+    public PhoneEditText(Context context) {
+        super(context);
+    }
+
+    public PhoneEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PhoneEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (s == null || s.length() == 0) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i != PHONE_INDEX_3 && i != PHONE_INDEX_8 && s.charAt(i) == ' ') {
+                continue;
+            } else {
+                sb.append(s.charAt(i));
+                if ((sb.length() == PHONE_INDEX_4 || sb.length() == PHONE_INDEX_9) && sb.charAt(sb.length() - 1) != ' ') {
+                    sb.insert(sb.length() - 1, ' ');
+                }
+            }
+        }
+        if (!sb.toString().equals(s.toString())) {
+            int index = start + 1;
+            if (sb.charAt(start) == ' ') {
+                if (before == 0) {
+                    index++;
+                } else {
+                    index--;
+                }
+            } else {
+                if (before == 1) {
+                    index--;
+                }
+            }
+
+            removeTextChangedListener(this);
+            setText(sb.toString());
+            setSelection(index);
+            addTextChangedListener(this);
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
